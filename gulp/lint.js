58afede62bf031d75deafec00899e196a15afd47
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-03 16:25:23 +0200

    lint task autofixes

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,15 +1,17 @@
-import gulp from 'gulp';
-import eslint from 'gulp-eslint';
+import gulp from "gulp";
+import eslint from "gulp-eslint";
+import cached from "gulp-cached";
 
-const allSrcGlob = [
-  'src/**/*.js',
-  'test/**/*.js'
-];
+const allSrcGlob = ["src/**/*.ts", "test/**/*.ts"];
 
 export const lint = () => {
-  return gulp.src(allSrcGlob)
-    .pipe(eslint())
-    .pipe(eslint.format());
+  return gulp
+    .src(allSrcGlob, { base: process.cwd() })
+    .pipe(cached("lint"))
+    .pipe(eslint({ fix: true }))
+    .pipe(eslint.format())
+    .pipe(cached("lint")) // Write only if something was fixed
+    .pipe(gulp.dest("."));
 };
 
-gulp.task('lint', lint);
+gulp.task("lint", lint);
