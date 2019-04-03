3da4c1382935d98cf530fc1d757f3a835b080950
Jason Lenoble <jason.lenoble@gmail.com>
2019-04-03 18:00:47 +0200

    Compile with gulp-typescript

--- gulp/dist.js
+++ gulp/dist.js
@@ -1,18 +1,32 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
+import gulp from "gulp";
+import ts from "gulp-typescript";
+import merge from "merge2";
 
-
-const libDir = 'lib';
-const srcGlob = [
-  'src/**/*.js'
-];
-
-export const dist = () => {
-  return gulp.src(srcGlob, {
-    since: gulp.lastRun(dist)
-  })
-    .pipe(babel())
-    .pipe(gulp.dest(libDir));
+const options = {
+  strict: true,
+  target: "esnext",
+  lib: ["esnext"],
+  module: "commonjs",
+  moduleResolution: "node",
+  declaration: true,
+  noEmitOnError: true,
+  noEmitHelpers: true
 };
 
-gulp.task('dist', dist);
+const libDir = "lib";
+const srcGlob = ["src/**/*.ts"];
+
+export const dist = () => {
+  const tsResult = gulp
+    .src(srcGlob, {
+      since: gulp.lastRun(dist)
+    })
+    .pipe(ts(options));
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest(libDir)),
+    tsResult.js.pipe(gulp.dest(libDir))
+  ]);
+};
+
+gulp.task("dist", dist);
